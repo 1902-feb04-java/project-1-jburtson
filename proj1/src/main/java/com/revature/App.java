49341301292a9420585c103f3f3dd0d11362b6ed
jburtson <jburtson@gmail.com>
2019-02-21 11:32:31 -0600

    Use scanner and BufferedReader to take in reimburshment request and login info from System.in

--- proj1/src/main/java/com/revature/App.java
+++ proj1/src/main/java/com/revature/App.java
@@ -1,4 +1,8 @@
 package com.revature;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -9,8 +13,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scan = new Scanner(System.in);
-        scan.close();
-        System.out.println("Yo, earth");
+        //  read a hypothetical reimbursement request or login from a client at the console.
+        // Scanner scan = new Scanner(System.in);
+        // scan.close();
+        try(
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+        ){
+            System.out.println("please input your reimbursement request information, seperated by \", \" \n(id, money, manager)");
+            String input = br.readLine();
+            System.out.println("You input: ["+input+"]");
+            Scanner scan = new Scanner(input);
+            scan.useDelimiter(", ");
+
+            String id = scan.next();
+            String money = scan.next();
+            String manager = scan.next();
+
+            System.out.println(id +" "+ money + " " + manager);
+
+            System.out.println("please input your username");
+            String username = br.readLine();
+            System.out.println("please input your password");
+            String password = br.readLine();
+
+            StringBuilder passwordHidden = new StringBuilder();
+            for (int i = 0; i<password.length(); i++) {
+                passwordHidden.append('*');
+            }
+            System.out.println("Username: "+username+"\nPassword: "+passwordHidden);
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch(NoSuchElementException e){
+            e.printStackTrace();
+        }
     }
 }

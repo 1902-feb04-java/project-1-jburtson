ac163af5f99a11e818fc86a8943445aaa347acbd
jburtson <jburtson@gmail.com>
2019-02-21 09:37:40 -0600

    added mvn project inside repository

---
+++ proj1/src/main/java/com/revature/App.java
@@ -0,0 +1,16 @@
+package com.revature;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        scan.close();
+        System.out.println("Yo, earth");
+    }
+}

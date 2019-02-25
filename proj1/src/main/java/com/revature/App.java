0fb4c740e4e0cc9eae5273e8616a3c9ac0be599e
jburtson <jburtson@gmail.com>
2019-02-25 15:24:28 -0600

    trying to connect to database

--- proj1/src/main/java/com/revature/App.java
+++ proj1/src/main/java/com/revature/App.java
@@ -1,7 +1,13 @@
 package com.revature;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -9,6 +15,22 @@ public class App
 {
     public static void main( String[] args )
     {
+        String url = "jdbc:postgresql://localhost:5432/postgres1";
+		String username = "postgres";
+        String password = "";
+        
+        try (Connection connection = DriverManager.getConnection(url,username,password)){
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT * FROM employees");
+			while (rs.next()) {
+				System.out.println(rs.getString("id"));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+        }
+        
+        
         Cli.run();
     }
 }
